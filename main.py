9f9bba59806d63b6ce23d7b9ea708a193e209ac2
mhurayr <128304499+mhurayr@users.noreply.github.com>
2023-03-19 17:46:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 
 # View our quick start guide to get your API key:
 # https://www.voiceflow.com/api/dialog-manager#section/Quick-Start
-api_key = "{api_key}"
+api_key = "{VF.DM.6415b59b17fda40007fe3a3a.evdv62LKAdRla1d4}"
 
 user_id = "user_123"  # Unique ID used to track conversation state
 user_input = "Hello world!"  # User's message to your Voiceflow assistant
